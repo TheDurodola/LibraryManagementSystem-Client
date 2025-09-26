132acdfd6a7c723872dc5b21b9ad6cf0a3ca13d3
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-09-26 18:47:32 +0100

    Added the extra fields for Sign Up

--- library-management-system/src/Hero/SignUpPageHero.jsx
+++ library-management-system/src/Hero/SignUpPageHero.jsx
@@ -7,8 +7,11 @@ function SignUpPageHero() {
         <div className={styles.inputbox}>
           <h2>Sign Up</h2>
           <form>
+            <input className={styles.input} type="text" name="firstname" id="firstname" placeholder="First Name"/>
+            <input className={styles.input} type="password" name="lastname" id="lastname" placeholder="Last name"/>
             <input className={styles.input} type="email" name="email" id="email" placeholder="Email"/>
-            <input className={styles.input} type="password" name="email" id="email" placeholder="Password"/>
+            <input className={styles.input} type="password" name="password" id="password" placeholder="Password"/>
+            <input className={styles.input} type="tel" name="phone" id="phone" placeholder="Phone Number"/>
             <button type="submit">Sign Up</button>
           </form>
         </div>
